af783b350084c416404585247921aacc3a6e6a8b
eunannana <thisiseunkim93@gmail.com>
2022-06-15 17:08:32 +0700

    design Form 2

--- ExeCRUDWinForm_136_A/Form2.cs
+++ ExeCRUDWinForm_136_A/Form2.cs
@@ -16,5 +16,10 @@ namespace ExeCRUDWinForm_136_A
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
